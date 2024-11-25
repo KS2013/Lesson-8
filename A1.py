411f8d1f95c8c70d2563f15d3658594abd6c3afd
KS2013 <ksarker2013@gmail.com>
2024-11-25 22:04:42 +0000

    Add files via upload

---
+++ A1.py
@@ -0,0 +1,27 @@
+# B - Brackets
+# I - Indecies
+# D - Division 
+# M - Multiplication
+# A - Addition
+# S - Subrtaction
+
+
+v = 4
+w = 5
+x = 8
+y = 2
+z = 0
+z = (v+w) * x/y
+print("(v+w) * x/ y is ", z)
+
+
+
+
+
+name = "Alex"
+age = 0
+
+if name == "Alex"  or name == "John" and age >= 2:
+    print("Hello! Welcome.")
+else:
+    print("Bye")
